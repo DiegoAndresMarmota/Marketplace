6b22f7ec3a69d3557110d63f34bd7fb3a6e72cfa
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-12 01:44:50 -0300

    Creación de form/login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,50 @@
-import { Button, Container } from "@mui/material";
+import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
 import React from "react";
 
 export const LoginPage: React.FC<{}> = () => {
     return (
-        <Container sx={{ mt: 9 }} maxWidth="xl">
-            <Button fullWidth variant='contained'>
-                Hola Login!
-            </Button>
+        <Container maxWidth="sm">
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{ minHeight: "100vh" }}
+            >
+                <Grid item>
+                    <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+                        <Typography
+                            variant="h4"
+                            sx={{ mt:1, mb:1 }}
+                        >
+                            Formulario
+                        </Typography>
+                        <Box component="form">
+                            <TextField
+                                margin="normal"
+                                fullWidth
+                                label="email" sx={{ mt: 2, mb: 1.5 }}
+                                required
+                            />
+                            <TextField
+                                margin="normal"
+                                fullWidth
+                                label="password"
+                                sx={{ mt: 1.5, mb: 1.5 }}
+                                required
+                            />
+                            <Button
+                                fullWidth
+                                type="submit"
+                                variant="contained"
+                                sx={{ mt:1.5, mb:3 }}
+                            >
+                                Iniciar Sesión
+                            </Button>
+                        </Box>
+                    </Paper>
+                </Grid>
+            </Grid>
         </Container>
-    )
-}
+    );
+};
