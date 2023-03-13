fe92e8ee2adf8287d60e77c64bc24861e1a26b6d
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-13 02:07:15 -0300

    Creación main/Alerts

--- src/config/theme.config.tsx
+++ src/config/theme.config.tsx
@@ -8,7 +8,9 @@ type ThemeProp = {
 export enum ThemePalette {
     BG = "#12181b",
     PRIMARY = "#C8FA5F",
-    FONT_GLOBAL = "'Montserrat', sans-serif"
+    FONT_GLOBAL = "'Montserrat', sans-serif",
+    ERROR_MAIN = "#f44336",
+    BG_ERROR_MAIN = "rgba(244, 67, 54, 0.1)"
 };
 
 const theme = createTheme({
@@ -33,9 +35,22 @@ const theme = createTheme({
                     borderRadius: "0.5em",
                 }
             }
-        }
-    }   
-})
+        },
+        MuiAlert: {
+            defaultProps: {
+                style: {
+                    borderRadius: "0.7em",
+                    fontSize: "1.1em",
+                },
+            },
+            styleOverrides: {
+                standardError: {
+                    border: `1px solid ${ThemePalette.ERROR_MAIN}`,
+                    backgroundColor: ThemePalette.BG_ERROR_MAIN,
+            },
+        },
+    },
+});
 
 export const ThemeConfig: React.FC<ThemeProp> = ({children}) => {
     return (
